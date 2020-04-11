b2b6f3e953cb0b39476f6a571c77cfdaf7621b77
fuad emon <fuadhasanemon8@gmail.com>
2020-04-11 22:57:57 +0600

    final proj

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
-// console.log('connected');
+// restart game button
+// console.log('conneted');
+var restart = document.querySelector("#b");
+
+//grab all the squares
+var squares = document.querySelectorAll("td");
+
+
+// cleae all the squares
+function clearBoard() {
+  for (var i = 0; i < squares.length; i++) {
+    squares[i].textContent = '';
+  }
+
+}
+
+restart.addEventListener('click',clearBoard);
+
+// check the square marker
+
+function changeMarker() {
+  if(this.textContent === '' ){
+    this.textContent = 'X';
+  } else if (this.textContent === 'X') {
+    this.textContent = 'O';
+  } else {
+    this.textContent = '';
+  }
+
+}
+
+for (var i = 0; i < squares.length; i++) {
+  squares[i].addEventListener('click',changeMarker)
+}
+
+
+
+// For loop to add event listeners to all the squares
